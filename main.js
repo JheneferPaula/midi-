dfb21ba0a2a1bc8bff19a5af9870057f01e47af6
Jhenefer Paula <115420131+JheneferPaula@users.noreply.github.com>
2023-01-25 21:50:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function tocaSom (selectorAudio){
+    const elemento = document.querySelector(selectorAudio);
+
+    if (elemento === null) {
+    //alert ('Elemento não encontrado');
+    console.log('Elemento não encontrado');
+    }
+
+    if (elemento != null) {
+        if (elemento.localName === 'audio') {
+            elemento.play();
+        }
+        
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+
+//enquanto
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+       
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
